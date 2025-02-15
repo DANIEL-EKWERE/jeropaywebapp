4cecc8ade85ad0a8a74be913e4ebdda8f103a945
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2025-02-15 17:45:51 +0100

    new release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1791105815"
+    serviceWorkerVersion: "3676316164"
   }
 });
